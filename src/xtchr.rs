2cfa07fc9f4d355bd2e84c9ffb7bbb933d5bb03a
SextantAI-Aleksandr <aleksandr@sextant-ai.com>
2023-05-23 22:17:22 -0400

    article_detail view

--- src/xtchr.rs
+++ src/xtchr.rs
@@ -65,37 +65,15 @@ impl Xtchr {
 
     /// Get one article, specified by id 
     pub async fn article_detail(&self, art_id: i32) -> Result<views::ArticleDetail, DiskError> {
-        let query = "
-        WITH paragraphs AS (
-            SELECT art_id, ARRAY_AGG(JSON_BUILD_OBJECT('prior_id', prior_id,
-                'content', JSON_BUILD_OBJECT('art_id', art_id, 'apara_id', apara_id, 'md', md),
-                'prior_sha256', prior_sha256, 'write_timestamp', write_timestamp, 'new_sha256', new_sha256
-            )) AS art_paras
-            FROM article_para GROUP BY art_id
-        ) SELECT ar.art_id, 
-        -- This JSON is for XtchdSQL<Author> which is converted to XtchdContent<Author> by the impl tokio_postgres::types::FromSql 
-        JSON_BUILD_OBJECT('prior_id', au.prior_id,
-            'content', JSON_BUILD_OBJECT('auth_id', au.auth_id, 'name', au.name),
-            'prior_sha256', au.prior_sha256, 'write_timestamp', au.write_timestamp, 'new_sha256', au.new_sha256
-        ) AS author, 
-        -- This JSON is for XtchdSQL<Article> which is converted to XtchdContent<Article> by the impl tokio_postgres::types::FromSql 
-        JSON_BUILD_OBJECT('prior_id', ar.prior_id,
-            'content', JSON_BUILD_OBJECT('art_id', ar.art_id, 'auth_id', ar.auth_id, 'title', ar.title),
-            'prior_sha256', ar.prior_sha256, 'write_timestamp', ar.write_timestamp, 'new_sha256', ar.new_sha256
-        ) AS article, 
-        p.art_paras
-        FROM articles ar LEFT JOIN authors au ON ar.auth_id = au.auth_id
-        LEFT JOIN paragraphs p ON ar.art_id = p.art_id
-        WHERE ar.art_id = $1
-        ";
+        let query = "SELECT author, article, art_paras FROM article_detail WHERE art_id = $1";
         let rows = self.c.query(query, &[&art_id]).await?;
         let row = match rows.get(0) {
             Some(val) => val,
             None => return Err(DiskError::missing_row())
         };
-        let author: XtchdContent<xrows::Author> = row.get(1);
-        let article: XtchdContent<xrows::Article> = row.get(2);
-        let paragraphs: Vec<XtchdContent<xrows::ArticlePara>> = row.get(3);
+        let author: XtchdContent<xrows::Author> = row.get(0);
+        let article: XtchdContent<xrows::Article> = row.get(1);
+        let paragraphs: Vec<XtchdContent<xrows::ArticlePara>> = row.get(2);
         Ok(views::ArticleDetail{author, article, paragraphs})
     }
 
--- views.sql
+++ views.sql
@@ -8,3 +8,28 @@ CREATE VIEW author_detail AS (
     FROM authorship
     INNER JOIN authors au ON authorship.auth_id = au.auth_id
 );
+
+
+CREATE VIEW article_detail AS (
+    WITH paragraphs AS (
+        SELECT art_id, ARRAY_AGG(JSON_BUILD_OBJECT('prior_id', prior_id,
+            'content', JSON_BUILD_OBJECT('art_id', art_id, 'apara_id', apara_id, 'md', md),
+            'prior_sha256', prior_sha256, 'write_timestamp', write_timestamp, 'new_sha256', new_sha256
+        )) AS art_paras
+        FROM article_para GROUP BY art_id
+    ) SELECT ar.art_id, 
+    -- This JSON is for XtchdSQL<Author> which is converted to XtchdContent<Author> by the impl tokio_postgres::types::FromSql 
+    JSON_BUILD_OBJECT('prior_id', au.prior_id,
+        'content', JSON_BUILD_OBJECT('auth_id', au.auth_id, 'name', au.name),
+        'prior_sha256', au.prior_sha256, 'write_timestamp', au.write_timestamp, 'new_sha256', au.new_sha256
+    ) AS author, 
+    -- This JSON is for XtchdSQL<Article> which is converted to XtchdContent<Article> by the impl tokio_postgres::types::FromSql 
+    JSON_BUILD_OBJECT('prior_id', ar.prior_id,
+        'content', JSON_BUILD_OBJECT('art_id', ar.art_id, 'auth_id', ar.auth_id, 'title', ar.title),
+        'prior_sha256', ar.prior_sha256, 'write_timestamp', ar.write_timestamp, 'new_sha256', ar.new_sha256
+    ) AS article, 
+    p.art_paras
+    FROM articles ar
+    LEFT JOIN authors au ON ar.auth_id = au.auth_id
+    LEFT JOIN paragraphs p ON ar.art_id = p.art_id
+);
